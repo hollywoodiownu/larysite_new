09431491ae75c2db2a6eebacaa284bff4436cadb
Hollywoodiownu / Lary Gary Mods <thecoltonjohn@gmail.com>
2023-12-04 16:43:38 -0700

    Update removed timer

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
 window.onload = function() {
-    // Countdown Timer Code
     var centerImageLink = document.getElementById('centerImageLink');
-    var countdown = document.getElementById('countdown');
 
     centerImageLink.classList.add('disabled');
     centerImageLink.querySelector('.center-img').addEventListener('animationend', function() {
         centerImageLink.classList.remove('disabled');
-        startCountdown(); 
     });
 
-    function startCountdown() {
-        var endDate = new Date('December 5, 2023 09:01:00 EST');
-        var interval = setInterval(function() {
-            var currentDate = new Date();
-            var timeLeft = endDate - currentDate;
-
-            if (timeLeft <= 0) {
-                clearInterval(interval);
-                countdown.innerHTML = '00:00:00';
-            } else {
-                var totalSeconds = Math.ceil(timeLeft / 1000);
-                var hours = Math.floor(totalSeconds / 3600);
-                var minutes = Math.floor((totalSeconds % 3600) / 60);
-                var seconds = totalSeconds % 60;
-
-                countdown.innerHTML = formatTime(hours) + ':' + formatTime(minutes) + ':' + formatTime(seconds);
-            }
-        }, 1000);
-    }
-
-    function formatTime(time) {
-        return time < 10 ? '0' + time : time;
-    }
-
-    // Audio Playback Functionality
+    // Audio Playback Functionality (Remains unchanged)
     var audioFiles = [
         'audio/song1.mp3',
         'audio/song2.mp3',
@@ -43,11 +16,11 @@ window.onload = function() {
 
     var currentAudioIndex = 0;
     var audio = new Audio();
-    audio.volume = 0.5; // Set volume to half
+    audio.volume = 0.5;
 
     function playNextAudio() {
         if (currentAudioIndex >= audioFiles.length) {
-            currentAudioIndex = 0; // Reset to the first file if we've reached the end
+            currentAudioIndex = 0;
         }
 
         audio.src = audioFiles[currentAudioIndex];
