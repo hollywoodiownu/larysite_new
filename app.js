50db2957248a76768c542a18333a2819ca1fe677
Hollywoodiownu / Lary Gary Mods <thecoltonjohn@gmail.com>
2023-12-04 05:09:37 -0700

    yep

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    // Countdown Timer Code
     var centerImageLink = document.getElementById('centerImageLink');
     var countdown = document.getElementById('countdown');
 
@@ -10,7 +11,6 @@ window.onload = function() {
 
     function startCountdown() {
         var endDate = new Date('December 5, 2023 09:01:00 EST');
-
         var interval = setInterval(function() {
             var currentDate = new Date();
             var timeLeft = endDate - currentDate;
@@ -29,8 +29,37 @@ window.onload = function() {
         }, 1000);
     }
 
-
     function formatTime(time) {
         return time < 10 ? '0' + time : time;
     }
+
+    // Audio Playback Functionality
+    var audioFiles = [
+        'audio/song1.mp3',
+        'audio/song2.mp3',
+        'audio/song3.mp3'
+        // Add more file paths as needed
+    ];
+
+    var currentAudioIndex = 0;
+    var audio = new Audio();
+    audio.volume = 0.5; // Set volume to half
+
+    function playNextAudio() {
+        if (currentAudioIndex >= audioFiles.length) {
+            currentAudioIndex = 0; // Reset to the first file if we've reached the end
+        }
+
+        audio.src = audioFiles[currentAudioIndex];
+        audio.play().then(() => {
+            console.log("Playing:", audioFiles[currentAudioIndex]);
+        }).catch(e => {
+            console.error("Failed to play:", e);
+        });
+
+        currentAudioIndex++;
+    }
+
+    audio.addEventListener('ended', playNextAudio);
+    playNextAudio(); // Start playing the first audio file
 };
