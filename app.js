f10f3cbe1474437ac9322f3142b44c53bfa6247a
Hollywoodiownu / Lary Gary Mods <thecoltonjohn@gmail.com>
2023-12-04 05:00:28 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,34 +2,34 @@ window.onload = function() {
     var centerImageLink = document.getElementById('centerImageLink');
     var countdown = document.getElementById('countdown');
 
-    // Initially set the links as disabled
     centerImageLink.classList.add('disabled');
-
-    // Wait for the fade-in animation to complete for the center image
     centerImageLink.querySelector('.center-img').addEventListener('animationend', function() {
         centerImageLink.classList.remove('disabled');
-        startCountdown(); // Start the countdown when the animation ends
+        startCountdown(); 
     });
 
-    // Countdown function
     function startCountdown() {
-        var endDate = new Date('2023-12-05T09:00:00Z'); // Set the end date and time
+        var endDate = new Date('December 5, 2023 09:01:00 EST');
+
         var interval = setInterval(function() {
             var currentDate = new Date();
             var timeLeft = endDate - currentDate;
+
             if (timeLeft <= 0) {
                 clearInterval(interval);
-                countdown.innerHTML = '00:00:00'; // Timer reached zero
+                countdown.innerHTML = '00:00:00';
             } else {
-                var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-                var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+                var totalSeconds = Math.ceil(timeLeft / 1000);
+                var hours = Math.floor(totalSeconds / 3600);
+                var minutes = Math.floor((totalSeconds % 3600) / 60);
+                var seconds = totalSeconds % 60;
+
                 countdown.innerHTML = formatTime(hours) + ':' + formatTime(minutes) + ':' + formatTime(seconds);
             }
         }, 1000);
     }
 
-    // Function to format time as "00"
+
     function formatTime(time) {
         return time < 10 ? '0' + time : time;
     }
