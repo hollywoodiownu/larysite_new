de9c923831b35de446de979fc1521045ed42bd61
Hollywoodiownu / Lary Gary Mods <thecoltonjohn@gmail.com>
2023-12-03 23:58:13 -0700

    Update

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 window.onload = function() {
-    var centerImgLink = document.getElementById('centerImageLink');
-    var centerImg = centerImgLink.querySelector('.center-img');
-    var kickImg = document.getElementById('kick-img');
-
-    // Wait for the fade-in animation to complete
-    centerImg.addEventListener('animationend', function() {
-        centerImgLink.classList.remove('disabled');
-    });
-
-    // Wait for the fade-in animation of the center image to complete
-    centerImg.addEventListener('animationend', function() {
-        kickImg.classList.remove('disabled');
-    });
+    var centerImageLink = document.getElementById('centerImageLink');
+    var kickLink = document.getElementById('kickLink');
 
     // Initially set the links as disabled
-    centerImgLink.classList.add('disabled');
-    kickImg.classList.add('disabled');
+    centerImageLink.classList.add('disabled');
+    kickLink.classList.add('disabled');
+
+    // Wait for the fade-in animation to complete for the center image
+    centerImageLink.querySelector('.center-img').addEventListener('animationend', function() {
+        centerImageLink.classList.remove('disabled');
+    });
+
+    // Wait for the fade-in animation to complete for the kick image
+    kickLink.querySelector('.kick-img').addEventListener('animationend', function() {
+        kickLink.classList.remove('disabled');
+    });
 };
