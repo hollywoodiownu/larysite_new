43d5736214e6f6a38b1874a7efee6503a067714e
Hollywoodiownu / Lary Gary Mods <thecoltonjohn@gmail.com>
2023-12-03 21:57:05 -0700

    yep

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 window.onload = function() {
     var centerImgLink = document.getElementById('centerImageLink');
-    var centerImg = centerImgLink.querySelector('img');
+    var centerImg = centerImgLink.querySelector('.center-img');
+    var kickImg = document.getElementById('kick-img');
 
     // Wait for the fade-in animation to complete
     centerImg.addEventListener('animationend', function() {
         centerImgLink.classList.remove('disabled');
     });
 
-    // Initially set the link as disabled
+    // Wait for the fade-in animation of the center image to complete
+    centerImg.addEventListener('animationend', function() {
+        kickImg.classList.remove('disabled');
+    });
+
+    // Initially set the links as disabled
     centerImgLink.classList.add('disabled');
+    kickImg.classList.add('disabled');
 };
