c241f3f0473e357aa981321fccb94bbe62cf0154
Hollywoodiownu / Lary Gary Mods <thecoltonjohn@gmail.com>
2023-12-04 04:21:30 -0700

    AnotherUpdate

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 window.onload = function() {
     var centerImageLink = document.getElementById('centerImageLink');
-    var kickLink = document.getElementById('kickLink');
+    var countdown = document.getElementById('countdown');
 
     // Initially set the links as disabled
     centerImageLink.classList.add('disabled');
-    kickLink.classList.add('disabled');
 
     // Wait for the fade-in animation to complete for the center image
     centerImageLink.querySelector('.center-img').addEventListener('animationend', function() {
         centerImageLink.classList.remove('disabled');
+        startCountdown(); // Start the countdown when the animation ends
     });
 
-    // Wait for the fade-in animation to complete for the kick image
-    kickLink.querySelector('.kick-img').addEventListener('animationend', function() {
-        kickLink.classList.remove('disabled');
-    });
+    // Countdown function
+    function startCountdown() {
+        var endDate = new Date('2023-12-05T09:00:00Z'); // Set the end date and time
+        var interval = setInterval(function() {
+            var currentDate = new Date();
+            var timeLeft = endDate - currentDate;
+            if (timeLeft <= 0) {
+                clearInterval(interval);
+                countdown.innerHTML = '00:00:00'; // Timer reached zero
+            } else {
+                var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+                countdown.innerHTML = formatTime(hours) + ':' + formatTime(minutes) + ':' + formatTime(seconds);
+            }
+        }, 1000);
+    }
+
+    // Function to format time as "00"
+    function formatTime(time) {
+        return time < 10 ? '0' + time : time;
+    }
 };
