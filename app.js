a0ce61224112b3d88b1dfd0ffe0048057216b03c
Hollywoodiownu / Lary Gary Mods <thecoltonjohn@gmail.com>
2023-12-12 22:46:42 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
-
-
-    // Audio Playback Functionality
-    var audioFiles = [
-        'audio/song1.mp3',
-        'audio/song2.mp3',
-        'audio/song3.mp3'
-        // Add more file paths as needed
-    ];
-
+window.onload = function() {
+    var audioFiles = ['audio/song1.mp3', 'audio/song2.mp3', 'audio/song3.mp3'];
     var currentAudioIndex = 0;
     var audioInitialized = false;
-    var audio;
-
-    function initializeAudio() {
-        audio = new Audio();
-        audio.volume = 0.5;
-        audio.src = audioFiles[currentAudioIndex];
-        audio.addEventListener('ended', playNextAudio);
-        audioInitialized = true;
-    }
+    var audio = new Audio();
+    audio.volume = 0.5;
 
     function playNextAudio() {
         currentAudioIndex = (currentAudioIndex + 1) % audioFiles.length;
@@ -27,28 +12,18 @@
     }
 
     function togglePlayPause() {
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
+        if (!audioInitialized) {
+            audio.src = audioFiles[currentAudioIndex];
+            audio.addEventListener('ended', playNextAudio);
+            audioInitialized = true;
         }
+        audio.paused ? audio.play() : audio.pause();
     }
 
-    // Keydown event listener for spacebar and right arrow
     window.addEventListener('keydown', function(e) {
-        if (e.keyCode === 32) { // Spacebar
+        if (e.keyCode === 32 || e.keyCode === 39) { // Spacebar or Right Arrow
             e.preventDefault();
-            if (!audioInitialized) {
-                initializeAudio();
-                audio.play();
-            } else {
-                togglePlayPause();
-            }
-        } else if (e.keyCode === 39) { // Right arrow key
-            e.preventDefault();
-            if (audioInitialized) {
-                playNextAudio();
-            }
+            togglePlayPause();
         }
     });
 };
