cf85fea6b9db15aef8652cd993f258061d0edd80
LaryGaryMods <73688953+hollywoodiownu@users.noreply.github.com>
2024-03-07 00:34:28 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 window.onload = function() {
-    var audioFiles = ['audio/song1.mp3', 'audio/song2.mp3', 'audio/song3.mp3'];
+    var audioFiles = ['H:\\larysite_new\\audio\\song1.mp3', 'H:\\larysite_new\\audio\\song2.mp3', 'H:\\larysite_new\\audio\\song3.mp3'];
     var currentAudioIndex = 0;
     var audioInitialized = false;
     var audio = new Audio();
     audio.volume = 0.5;
+
+    var duckSound = new Audio('H:\\larysite_new\\audio\\ducksoundnew.mp3');
 
     function playNextAudio() {
         currentAudioIndex = (currentAudioIndex + 1) % audioFiles.length;
@@ -20,21 +22,31 @@ window.onload = function() {
         audio.paused ? audio.play() : audio.pause();
     }
 
-    // Disable Ctrl+Plus, Ctrl+Minus, Ctrl+Mousewheel for zoom control
+    // Play the duck sound when the duck image is clicked
+    document.querySelector('.duck').addEventListener('click', function() {
+        duckSound.play();
+    });
+
+    // Space key to toggle play/pause
+    document.addEventListener('keydown', function(e) {
+        if (e.code === 'Space') {
+            togglePlayPause();
+        }
+    });
+
+    // Disable zoom controls
     document.addEventListener('keydown', function(e) {
         if ((e.ctrlKey === true || e.metaKey === true) && (e.which === 61 || e.which === 107 || e.which === 173 || e.which === 109 || e.which === 187  || e.which === 189 )) {
             e.preventDefault();
         }
     }, false);
 
-    // Disable pinch zoom
     document.addEventListener('wheel', function(e) {
         if (e.ctrlKey === true) {
             e.preventDefault();
         }
     }, { passive: false });
 
-    // Disable double-tap zoom for touch devices
     let lastTouchEnd = 0;
     document.addEventListener('touchend', function(e) {
         const now = (new Date()).getTime();
